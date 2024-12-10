04b761c2884aaec97611399503e59791b3d2ba4a
Nisa Parveen Shaikh <kuroibarakarose@gmail.com>
2024-12-10 15:46:36 +0530

    Update Todos.tsx to Remove Todos

--- TodoList/src/components/Todos.tsx
+++ TodoList/src/components/Todos.tsx
@@ -3,11 +3,11 @@ import Todo from '../models/todo.ts';
 import TodoItem from './TodoItem.tsx';
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[], onRemove: (id: string) => void }> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map((item) => (
-                <TodoItem key={item.id} text={item.text} />
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemove.bind(null, item.id)} />
             ))}
         </ul>
     )
